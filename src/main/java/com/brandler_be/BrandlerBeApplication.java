824e71adbe9f36be8f1609ab38cc20b94dfd28a4
kohj-git <22000071@handong.ac.kr>
2025-07-08 19:15:38 +0900

    초기 프로젝트 설정 및 .gitignore 업데이트

---
+++ src/main/java/com/brandler_be/BrandlerBeApplication.java
@@ -0,0 +1,13 @@
+package com.brandler_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandlerBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrandlerBeApplication.class, args);
+    }
+
+}
